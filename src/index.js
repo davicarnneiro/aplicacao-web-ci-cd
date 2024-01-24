db122699e66030bfc54f6e1498b6f51b0039e300
Davi <edson.carneiro-consultmidia@pbh.gov.br>
2024-01-24 15:16:57 -0300

    Deploy Kubernetes - Correção

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 8080
+// const express = require('express')
+// const app = express()
+// const port = 8080
 
-app.get("/", function(req, res){
-  res.sendFile(__dirname + "/index.html");
-});
+// app.get("/", function(req, res){
+//   res.sendFile(__dirname + "/index.html");
+// });
 
-app.listen(port, () => {
-  console.log(`Aplicação sendo executada na porta ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Aplicação sendo executada na porta ${port}`)
+// })
 
